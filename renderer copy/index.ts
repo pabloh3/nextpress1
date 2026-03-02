30ade3b1252ae0a550aaa2fde01dffc132677a06
Husseinkizz <hssnkizz@gmail.com>
2026-03-02 10:36:17 +0300

    refactor(shared): removed some redundant files

--- renderer copy/index.ts
+++
@@ -1,4 +0,0 @@
-export { renderBlocksToHtml, getHydrationScript } from "./to-html";
-export { PageTemplate } from "./templates/page";
-export type { BlockData } from "./react/block-types";
-export { BLOCK_COMPONENTS } from "./react/block-components";
